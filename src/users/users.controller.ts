14b9ad5859b00d522c877e098c305493e7848d1c
shuty <shuty@shuty-no-iMac.local>
2022-03-23 17:02:57 +0900

    sample

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import { UsersService } from './users.service';
         type: User
     }
 })
+
 @Controller('users')
 export class UsersController implements CrudController<User> {
     constructor(public service: UsersService) {}
